d844d9912075f839ae1bf9b4704273997280b877
Rafal <rafal.araszkiewicz@student.put.poznan.pl>
2015-09-23 16:47:58 +0200

    #0.0.2 Select target command added. Library required for directory selection dialog added. RelayCOmmand added.

---
+++ DirectoryJunctionProject/ViewModel/DirJunctionViewModel.cs
@@ -0,0 +1,76 @@
+﻿using DirectoryJunctionProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace DirectoryJunctionProject.ViewModel
+{
+    class DirJunctionViewModel: INotifyPropertyChanged
+    {
+        public string LinkName { get; set; } = "Link name";
+
+        public string LinkDirectoryPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        private string _targetPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public string TargetPath
+        {
+            get
+            {
+                return _targetPath;
+            }
+            set
+            {
+                _targetPath = value;
+                OnPropertyChanged(nameof(TargetPath));
+            }
+        }
+
+        private CommonFileDialog _folderDialog;
+
+        public DirJunctionViewModel()
+        {
+            _folderDialog = new CommonOpenFileDialog()
+            {
+                IsFolderPicker = true,
+                DefaultDirectory = TargetPath,
+            };
+
+            CreateSelectTargetCommand();
+        }
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler eventHandler = PropertyChanged;
+            eventHandler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion 
+
+
+        #region Commands
+
+        public ICommand SelectTargetCommand { get; private set; }
+
+        private void CreateSelectTargetCommand()
+        {
+            SelectTargetCommand = new RelayCommand<object>(SelectTargetExecute);
+        }
+
+        public void SelectTargetExecute(object dummy)
+        {
+            if (_folderDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                TargetPath = _folderDialog.FileName;
+        }
+        #endregion
+    }
+}
